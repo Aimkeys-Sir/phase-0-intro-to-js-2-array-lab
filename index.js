29ae38964302bf96141b0c53a866bfb23420696c
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-06-10 10:03:13 +0300

    commit tree b18ddc0a09670da451f924de907d1ab9e60d8b16

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name="Ralph")
+{
+    cats.push(name);
+}
+function destructivelyPrependCat(name="Guy")
+{
+cats.unshift(name);
+}
+function destructivelyRemoveLastCat()
+{
+    cats.pop();
+}
+function destructivelyRemoveFirstCat()
+{
+    cats.shift();
+}
+function appendCat(name="Ralph")
+{
+    return [...cats,name];
+}
+function prependCat(name="Guy")
+{
+    return [name,...cats];
+}
+function removeLastCat()
+{
+    return cats.slice(0,-1);
+}
+function removeFirstCat()
+{
+    return cats.slice(1);
+}
